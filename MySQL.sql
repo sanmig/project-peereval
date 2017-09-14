73f35c19596d9decd02ea034cdbfa9f3abf225e3
sanmig <Karl.Pelayo@it.weltec.ac.nz>
2017-09-14 15:50:40 +1200

    added time,code,option

--- MySQL.sql
+++ MySQL.sql
@@ -1,5 +1,9 @@
 SET FOREIGN_KEY_CHECKS= 0;
 
+CREATE DATABASE peereval CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+use peereval;
+
 CREATE TABLE IF NOT EXISTS user_main (
 id bigint(20) NOT NULL AUTO_INCREMENT,
 username varchar(20) NOT NULL,
@@ -9,6 +13,8 @@ firstName varchar(20) NOT NULL,
 lastName varchar(20) NOT NULL,
 role enum( 'ROLE_USER' , 'ROLE_ADMIN' ) NOT NULL,
 isVerified smallint(1) NOT NULL DEFAULT 0,
+verifyCode varchar(255) DEFAULT NULL,
+registerAt datetime,
 PRIMARY KEY (id),
 UNIQUE KEY username (username),
 UNIQUE KEY email (email)
@@ -24,47 +30,57 @@ PRIMARY KEY (id),
 UNIQUE KEY weltecId (weltecId)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS question_main (
+CREATE TABLE IF NOT EXISTS student_evaluate (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-text text NOT NULL,
-PRIMARY KEY (id)
+studentId bigint(20) NOT NULL,
+formId bigint(20) NOT NULL,
+registerAt datetime,
+PRIMARY KEY (id),
+FOREIGN KEY (studentId) REFERENCES student_main(id),
+FOREIGN KEY (formId) REFERENCES evaluation_form(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS evaluation_form (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-evalQuestion bigint(20) NOT NULL,
+tutorId bigint(20) NOT NULL,
+description text NOT NULL,
+createdAt datetime,
+expiryAt datetime,
 PRIMARY KEY (id),
-FOREIGN KEY (evalQuestion) REFERENCES evaluation_question(id)
+FOREIGN KEY (tutorId) REFERENCES user_main(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS evaluation_question (
 id bigint(20) NOT NULL AUTO_INCREMENT,
 tutorId bigint(20) NOT NULL,
 questId bigint(20) NOT NULL,
+formId bigint(20) NOT NULL,
+registerAt datetime,
 PRIMARY KEY (id),
 FOREIGN KEY (tutorId) REFERENCES user_main(id),
-FOREIGN KEY (questId) REFERENCES question_main(id)
+FOREIGN KEY (questId) REFERENCES question_main(id),
+FOREIGN KEY (formId) REFERENCES evaluation_form(id)
+)ENGINE=InnoDB CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS question_main (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+text text NOT NULL,
+PRIMARY KEY (id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS evaluation_answer (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-weltecId bigint(20) NOT NULL,
-evalId bigint(20) NOT NULL,
+formId bigint(20) NOT NULL,
+studentId bigint(20) NOT NULL,
+questionId bigint(20) NOT NULL,
 answer smallint(5) NOT NULL,
+status smallint(1) NOT NULL,
+formCode varchar(255) DEFAULT NULL,
+attemptAt datetime,
 PRIMARY KEY (id),
-FOREIGN KEY (weltecId) REFERENCES student_main(id),
-FOREIGN KEY (evalId) REFERENCES evaluation_form(id)
-)ENGINE=InnoDB CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS evaluation_review (
-id bigint(20) NOT NULL AUTO_INCREMENT,
-evalForm bigint(20) NOT NULL,
-evalQuestion bigint(20) NOT NULL,
-evalAnswer bigint(20) NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (evalForm) REFERENCES evaluation_form(id),
-FOREIGN KEY (evalQuestion) REFERENCES evaluation_question(id),
-FOREIGN KEY (evalAnswer) REFERENCES evaluation_answer (id)
+FOREIGN KEY (formId) REFERENCES evaluation_form(id),
+FOREIGN KEY (studentId) REFERENCES student_main (id),
+FOREIGN KEY (questionId) REFERENCES question_main(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 SET FOREIGN_KEY_CHECKS= 1;
