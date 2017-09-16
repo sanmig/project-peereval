2ac068c4bb098b316bc2760bacca5e63010ca81c
sanmig <Karl.Pelayo@it.weltec.ac.nz>
2017-09-16 16:35:25 +1200

    v4

--- MySQL.sql
+++ MySQL.sql
@@ -32,59 +32,59 @@ UNIQUE KEY weltecId (weltecId)
 
 CREATE TABLE IF NOT EXISTS student_evaluate (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-student bigint(20) NOT NULL,
-form bigint(20) NOT NULL,
+forms bigint(20) NOT NULL,
+students bigint(20) NOT NULL,
 registerAt datetime,
 PRIMARY KEY (id),
-FOREIGN KEY (student) REFERENCES student_main(id),
-FOREIGN KEY (form) REFERENCES evaluation_form(id)
+FOREIGN KEY (forms) REFERENCES evaluation_form(id),
+FOREIGN KEY (students) REFERENCES student_main(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS evaluation_form (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-user bigint(20) NOT NULL,
+users bigint(20) NOT NULL,
 courseName varchar(255) NOT NULL,
 courseCode varchar(20) NOT NULL,
 description varchar(255) NOT NULL,
 createdAt datetime,
 expiryAt datetime,
 PRIMARY KEY (id),
-FOREIGN KEY (user) REFERENCES user_main(id)
+FOREIGN KEY (users) REFERENCES user_main(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS evaluation_question (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-user bigint(20) NOT NULL,
-form bigint(20) NOT NULL,
-question bigint(20) NOT NULL,
+users bigint(20) NOT NULL,
+forms bigint(20) NOT NULL,
+questions bigint(20) NOT NULL,
 registerAt datetime,
 PRIMARY KEY (id),
-FOREIGN KEY (user) REFERENCES user_main(id),
-FOREIGN KEY (form) REFERENCES evaluation_form(id),
-FOREIGN KEY (question) REFERENCES question_main(id)
+FOREIGN KEY (users) REFERENCES user_main(id),
+FOREIGN KEY (forms) REFERENCES evaluation_form(id),
+FOREIGN KEY (questions) REFERENCES question_main(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS question_main (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-question text NOT NULL,
+questionText text NOT NULL,
 PRIMARY KEY (id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS evaluation_answer (
 id bigint(20) NOT NULL AUTO_INCREMENT,
-form bigint(20) NOT NULL,
-student bigint(20) NOT NULL,
-question bigint(20) NOT NULL,
-answer bigint(20) NOT NULL,
+forms bigint(20) NOT NULL,
+students bigint(20) NOT NULL,
+questions bigint(20) NOT NULL,
+answers bigint(20) NOT NULL,
 status smallint(1) NOT NULL DEFAULT 0,
 formCode varchar(255) DEFAULT NULL,
 feedback text,
 attemptAt datetime,
 PRIMARY KEY (id),
-FOREIGN KEY (form) REFERENCES evaluation_form(id),
-FOREIGN KEY (student) REFERENCES student_main (id),
-FOREIGN KEY (question) REFERENCES question_main(id),
-FOREIGN KEY (answer) REFERENCES answer_main(id)
+FOREIGN KEY (forms) REFERENCES evaluation_form(id),
+FOREIGN KEY (students) REFERENCES student_main (id),
+FOREIGN KEY (questions) REFERENCES question_main(id),
+FOREIGN KEY (answers) REFERENCES answer_main(id)
 )ENGINE=InnoDB CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS answer_main (
